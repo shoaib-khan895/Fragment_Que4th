bb54d5464fba2d4adefddc21b671b9a7c9a60290
shoaib <mohshoaibkhan0@gmail.com>
2021-03-07 20:22:41 +0530

    second commit

--- app/src/main/java/com/example/fragment_question4/Frag_First.kt
+++ app/src/main/java/com/example/fragment_question4/Frag_First.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.fragment_frag_first.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,7 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Frag_1.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Frag_1 : Fragment() {
+class Frag_1 : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
